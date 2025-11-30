735da03673e613bf9fb8e6dba500d7a831e88ea0
was9qte <115586331+was9qte@users.noreply.github.com>
2025-12-01 00:43:56 +0300

    Add files via upload

---
+++ Проекты/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+private void dtpBirthDate_ValueChanged(object sender, EventArgs e)
+        {
+            int age = DateTime.Today.Year - dtpBirthDate.Value.Year;
+            if (dtpBirthDate.Value > DateTime.Today.AddYears(-age))
+                age--;
+            lblAge.Text = $"Возраст: {age} лет";
+        }
+        private void trackBarTemp_Scroll(object sender, EventArgs e)
+        {
+            txtTemp.Text = trackBarTemp.Value.ToString();
+        }
+        private void nudRadius_ValueChanged(object sender, EventArgs e)
+        {
+            double r = (double)nudRadius.Value;
+            double area = 4 * Math.PI * r * r;
+            double volume = (4.0 / 3.0) * Math.PI * r * r * r;
+
+            lblSurfaceArea.Text = $"Площадь: {Math.Round(area, 2)} м²";
+            lblVolume.Text = $"Объём: {Math.Round(volume, 2)} м³";
+        }
+
+    }
+}
+
---
+++ Проекты/WindowsFormsApp1/WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("WindowsFormsApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("219e4320-4d53-4b44-8338-f89f8c151c48")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
