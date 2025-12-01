20b46d8293afc625c1c653f6ea9819bf7ba3a45d
was9qte <115586331+was9qte@users.noreply.github.com>
2025-12-01 12:08:14 +0300

    Add files via upload

---
+++ lab 29/labwork/labwork/Programm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace labwork
+{
+    partial class Project
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FontToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.DefaultFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.fontDialog = new System.Windows.Forms.FontDialog();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.SettingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenToolStripMenuItem,
+            this.SaveToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.FileToolStripMenuItem.Text = "Файл";
+            // 
+            // OpenToolStripMenuItem
+            // 
+            this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.OpenToolStripMenuItem.Text = "Открыть";
+            this.OpenToolStripMenuItem.Click += new System.EventHandler(this.OpenFileDialog_Click);
+            // 
+            // SaveToolStripMenuItem
+            // 
+            this.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem";
+            this.SaveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.SaveToolStripMenuItem.Text = "Сохранить";
+            this.SaveToolStripMenuItem.Click += new System.EventHandler(this.SaveFileDialog_Click);
+            // 
+            // SettingsToolStripMenuItem
+            // 
+            this.SettingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ColorToolStripMenuItem,
+            this.FontToolStripMenuItem1,
+            this.DefaultFolderToolStripMenuItem});
+            this.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem";
+            this.SettingsToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.SettingsToolStripMenuItem.Text = "Настройки";
+            // 
+            // ColorToolStripMenuItem
+            // 
+            this.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem";
+            this.ColorToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.ColorToolStripMenuItem.Text = "Цвет";
+            this.ColorToolStripMenuItem.Click += new System.EventHandler(this.SettingsColor_Click);
+            // 
+            // FontToolStripMenuItem1
+            // 
+            this.FontToolStripMenuItem1.Name = "FontToolStripMenuItem1";
+            this.FontToolStripMenuItem1.Size = new System.Drawing.Size(194, 22);
+            this.FontToolStripMenuItem1.Text = "Шрифт";
+            this.FontToolStripMenuItem1.Click += new System.EventHandler(this.FontDialog_Apply);
+            // 
+            // DefaultFolderToolStripMenuItem
+            // 
+            this.DefaultFolderToolStripMenuItem.Name = "DefaultFolderToolStripMenuItem";
+            this.DefaultFolderToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.DefaultFolderToolStripMenuItem.Text = "Папка по умолчанию";
+            this.DefaultFolderToolStripMenuItem.Click += new System.EventHandler(this.FolderBrowserDialog_HelpRequest_1);
+            // 
+            // RichTextBox
+            // 
+            this.RichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RichTextBox.Location = new System.Drawing.Point(0, 24);
+            this.RichTextBox.Name = "RichTextBox";
+            this.RichTextBox.Size = new System.Drawing.Size(800, 426);
+            this.RichTextBox.TabIndex = 1;
+            this.RichTextBox.Text = "";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Filter = "Текстовые документы (*.txt)|*.txt|Текст в формате rtf (*.rtf)|*.rtf|Все файлы (*." +
+    "*)|*.*";
+            this.openFileDialog.Title = "Открыть файл";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "txt";
+            this.saveFileDialog.Filter = "Текстовые документы (*.txt)|*.txt|Текст в формате rtf (*.rtf)|*.rtf|Все файлы (*." +
+    "*)|*.*";
+            this.saveFileDialog.Title = "Сохранить файл";
+            // 
+            // fontDialog
+            // 
+            this.fontDialog.Apply += new System.EventHandler(this.FontDialog_Apply);
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.Description = "Выберите папку по умолчанию";
+            this.folderBrowserDialog.HelpRequest += new System.EventHandler(this.FolderBrowserDialog_HelpRequest_1);
+            // 
+            // Project
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RichTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Project";
+            this.Text = "Текстовый документ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FontToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem DefaultFolderToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox RichTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.FontDialog fontDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ lab 29/labwork/labwork/Programm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labwork
+{
+    public partial class Project : Form
+    {
+        public Project()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void FontDialog_Apply(object sender, EventArgs e)
+        {
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                RichTextBox.SelectionFont = fontDialog.Font;
+            }
+        }
+        private void SettingsColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                RichTextBox.SelectionColor = colorDialog.Color;
+            }
+        }
+        private void FolderBrowserDialog_HelpRequest_1(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedPath = folderBrowserDialog.SelectedPath;
+                openFileDialog.InitialDirectory = selectedPath;
+                saveFileDialog.InitialDirectory = selectedPath;
+            }
+        }
+
+        private void SaveFileDialog_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                if (filePath.EndsWith(".rtf"))
+                {
+                    RichTextBox.SaveFile(filePath, RichTextBoxStreamType.RichText);
+                }
+                else
+                {
+                    RichTextBox.SaveFile(filePath, RichTextBoxStreamType.PlainText);
+                }
+            }
+        }
+
+        private void OpenFileDialog_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                if (filePath.EndsWith(".rtf"))
+                {
+                    RichTextBox.LoadFile(filePath, RichTextBoxStreamType.RichText);
+                }
+                else
+                {
+                    RichTextBox.LoadFile(filePath, RichTextBoxStreamType.PlainText);
+                }
+            }
+        }
+    }
+}
